2384dfdeb52bf9b40dd5f0845a297d4e17fc050b
Ignacio Marcos Serrano <imarcoss@cnic-e894789x6305681.cnic.es>
2026-08-19 17:42:51 +0200

    DOCs

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Nvitk'
+copyright = '2026, Ignacio Marcos Serrano'
+author = 'Ignacio Marcos Serrano'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
 extensions = ["autoapi.extension", "myst_parser", "sphinx_click", "sphinx.ext.napoleon"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
 autoapi_dirs = ["../src/nvitk"]
 autoapi_type = "python"
 napoleon_numpy_docstring = True
